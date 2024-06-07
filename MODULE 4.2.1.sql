8ea487508feb1ec193771f81ab4921375c5b395d
TopsCode <kailash.solanki@tops-int.com>
2024-06-07 13:25:55 +0530

    Add files via upload

--- MODULE 4.2.1.sql
+++ MODULE 4.2.1.sql
@@ -1,4 +1,38 @@
 -- string functions in sql-- 
+-- Create a table with columns for each string data type
+CREATE TABLE string_data_types (
+    char_col CHAR(10),
+    varchar_col VARCHAR(255),
+    tinytext_col TINYTEXT,
+    text_col TEXT,
+    mediumtext_col MEDIUMTEXT,
+    longtext_col LONGTEXT,
+    binary_col BINARY(16),
+    varbinary_col VARBINARY(255),
+    enum_col ENUM('value1', 'value2', 'value3'),
+    set_col SET('value1', 'value2', 'value3')
+);
+
+-- Insert sample data into the table
+INSERT INTO string_data_types (
+    char_col,
+    varchar_col,
+    tinytext_col,
+    text_col,
+    mediumtext_col,
+    longtext_col,
+    binary_col,
+    varbinary_col,
+    enum_col,
+    set_col
+) VALUES (
+    'char_data',
+    'This is a varchar data',
+    'tiny text data',
+    'This is a longer text data that goes into TEXT column.',
+    'This is a medium text data that goes into MEDIUMTEXT column. It is usually longer than TEXT data type.',
+    '
+
 
 -- concat  
 SELECT CONCAT('Hello', ' ', 'World') AS ConcatenatedString;
